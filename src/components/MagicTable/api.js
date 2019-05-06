f707a50ee3931583706a65eaab9906b105732e7e
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-05 17:33:42 -0700

    Changes to use Rx method

--- src/components/MagicTable/api.js
+++ src/components/MagicTable/api.js
@@ -1,12 +1,10 @@
 import { of, from } from "rxjs";
-import { map, switchMapTo, mergeMap } from "rxjs/operators";
+import { map, mergeMap, startWith } from "rxjs/operators";
 import { getRowsBetween, getNumberOfRows } from "@/src/data/get-rows-api";
 
-const numberOfRows$ = of(0).pipe(
-  switchMapTo(
-    from(getNumberOfRows())
-  ),
-);
+const numberOfRows$ = from(getNumberOfRows()).pipe(
+  startWith(0)
+)
 
 // Assume all rows for now
 const rowRequest$ = numberOfRows$.pipe(
