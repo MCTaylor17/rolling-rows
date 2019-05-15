c73f18ef8e6cf4507ebb907e805fb6f2ef84898e
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-15 02:32:56 -0700

    Adds middleRow indicator

--- src/components/MagicTable/Hud.js
+++ src/components/MagicTable/Hud.js
@@ -6,9 +6,20 @@ const Hud = props => {
   const styles = {
     "--furthestRow": props.furthestRow
   }
+  const middle = {
+    "--furthestRow": props.middleRow
+  }
+  const middleRow = () => {
+    if(props.middleRow < props.furthestRow) {
+      return <div className="middle-row" style={middle}>{props.middleRow}</div>
+    }
+    return null;
+  }
   return (
     <>
       <div className="furthest-row" style={styles}>{props.furthestRow}</div>
+    
+      {middleRow()}
     </>
   );
 };
