81275c3e6f94761eb93703ab4aa5d77e14431bd2
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-20 15:02:35 -0700

    Splits content-blocks into partials

--- src/styles/index.js
+++ src/styles/index.js
@@ -3,8 +3,10 @@ import "./partials/norms.scss";
 
 // Global
 import "./partials/typography.scss";
+import "./partials/layout.scss";
 
 // Specific
+import "./partials/header.scss";
 import "./partials/background.scss";
 import "./partials/background-buttons.scss";
-import "./partials/content-blocks.scss";
+import "./partials/failure.scss";
