7b0a64a686ac8ac8523715bbe73def6ecbaa2fde
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-20 14:49:55 -0700

    Rollup is stupid and ignorant
    It's too stupid to know when I've updated files
    imported through @import statements within sass.

---
+++ src/styles/index.js
@@ -0,0 +1,10 @@
+// Setup
+import "./partials/norms.scss";
+
+// Global
+import "./partials/typography.scss";
+
+// Specific
+import "./partials/background.scss";
+import "./partials/background-buttons.scss";
+import "./partials/content-blocks.scss";
