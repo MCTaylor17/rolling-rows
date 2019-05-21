b7b88aac3ebf46e6a21f745695bce817cd17a9c7
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-20 19:24:14 -0700

    Improves splash layout

--- src/styles/index.js
+++ src/styles/index.js
@@ -6,7 +6,7 @@ import "./partials/typography.scss";
 import "./partials/layout.scss";
 
 // Specific
-import "./partials/header.scss";
 import "./partials/background.scss";
 import "./partials/background-buttons.scss";
+import "./partials/splash-header.scss";
 import "./partials/failure.scss";
