265f45697d772229d841b48a2fb04dc3a11e5944
MCTaylor17 <MCTaylor17@gmail.com>
2019-05-20 19:29:10 -0700

    Squashed commit of the following:
    Robust header splash layout:
    
    commit cd5e230d62c09dae2d1a82c9915d52b0fb9129b6
    Author: MCTaylor17 <MCTaylor17@gmail.com>
    Date:   Mon May 20 19:28:21 2019 -0700
    
        Finishing touches
    
    commit b7b88aac3ebf46e6a21f745695bce817cd17a9c7
    Author: MCTaylor17 <MCTaylor17@gmail.com>
    Date:   Mon May 20 19:24:14 2019 -0700
    
        Improves splash layout
    
    commit 11ffb02221fc005d0708c7312b0ba150d0d6864c
    Author: MCTaylor17 <MCTaylor17@gmail.com>
    Date:   Mon May 20 15:33:47 2019 -0700
    
        Adds a global import
    
        This is a rollup workaround since sass imports from js
        don't pass variables forward to new imports.
    
    commit 81275c3e6f94761eb93703ab4aa5d77e14431bd2
    Author: MCTaylor17 <MCTaylor17@gmail.com>
    Date:   Mon May 20 15:02:35 2019 -0700
    
        Splits content-blocks into partials
    
    commit 7b0a64a686ac8ac8523715bbe73def6ecbaa2fde
    Author: MCTaylor17 <MCTaylor17@gmail.com>
    Date:   Mon May 20 14:49:55 2019 -0700
    
        Rollup is stupid and ignorant
    
        It's too stupid to know when I've updated files
        imported through @import statements within sass.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 console.log("Hello World");
-import "./styles/main.scss";
+import "./styles";
 import React from "react";
 import ReactDOM from "react-dom";
 import MagicTable from "@/src/components/MagicTable";
---
+++ src/styles/index.js
@@ -0,0 +1,12 @@
+// Setup
+import "./partials/norms.scss";
+
+// Global
+import "./partials/typography.scss";
+import "./partials/layout.scss";
+
+// Specific
+import "./partials/background.scss";
+import "./partials/background-buttons.scss";
+import "./partials/splash-header.scss";
+import "./partials/failure.scss";
